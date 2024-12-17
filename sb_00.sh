19d430cefbfa1d3ce4ee081f7af12297fb09d643
zhenzhenjunzilu <zhenzhenjunzilu@gmail.com>
2024-12-17 18:52:06 +0800

    Update sb_00.sh

--- sb_00.sh
+++ sb_00.sh
@@ -13,7 +13,7 @@ reading() { read -p "$(red "$1")" "$2"; }
 export LC_ALL=C
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'bc97f674-c578-4940-9234-0a1da46041b0'}
+export UUID=${UUID:-'4643d27d-facd-44cb-9cb8-90a7f8e892b6'}
 export NEZHA_SERVER=${NEZHA_SERVER:-''} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''} 
